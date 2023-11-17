f1bbcf926c511598d9cb1b367832143074118cf4
marlon1925 <marlon.lalangui@epn.edu.ec>
2023-11-17 14:36:34 -0500

    oring

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import { Camera, CameraResultType, CameraSource, Photo} from '@capacitor/camera';
+import {Filesystem, Directory} from "@capacitor/filesystem"
+import {Preferences} from "@capacitor/preferences"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor() { }
+}
